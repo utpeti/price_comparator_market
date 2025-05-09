6946590a5da5277c8ba8ed6f950c69a8a3005b1f
diboti <korposb@gmail.com>
2025-05-09 23:27:09 +0300

    first commit

---
+++ src/main/java/com/diboti/pricecomparatormarket/PriceComparatorMarketApplication.java
@@ -0,0 +1,13 @@
+package com.diboti.pricecomparatormarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PriceComparatorMarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PriceComparatorMarketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/diboti/pricecomparatormarket/PriceComparatorMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diboti.pricecomparatormarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PriceComparatorMarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
