14b7893bb029ec2298d996f6caa795c1cabfc3ce
diboti <korposb@gmail.com>
2025-05-21 17:02:40 +0300

    add prev changes

---
+++ src/main/java/com/diboti/pricecomparatormarket/controller/DiscountController.java
@@ -0,0 +1,11 @@
+package com.diboti.pricecomparatormarket.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/v1/discounts")
+public class DiscountController {
+}
