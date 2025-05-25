d48b4bf7d51e0ea6007665c7938faa071444fefd
diboti <korposb@gmail.com>
2025-05-25 13:38:45 +0300

    constrains exception handling

--- src/main/java/com/diboti/pricecomparatormarket/controller/exceptions/utils/GeneralExceptionHandler.java
+++ src/main/java/com/diboti/pricecomparatormarket/controller/exceptions/utils/GeneralExceptionHandler.java
@@ -4,10 +4,14 @@ import com.diboti.pricecomparatormarket.controller.exceptions.BadRequestExceptio
 import com.diboti.pricecomparatormarket.controller.exceptions.NotFoundException;
 import com.diboti.pricecomparatormarket.controller.exceptions.ServerErrorException;
 import com.diboti.pricecomparatormarket.dto.outgoing.ErrorDto;
+import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class GeneralExceptionHandler {
@@ -30,4 +34,19 @@ public class GeneralExceptionHandler {
         return new ErrorDto(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
     }
 
+    @ExceptionHandler(ConstraintViolationException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public final Iterable<String> handleConstraintViolation(ConstraintViolationException e) {
+        return e.getConstraintViolations().stream()
+                .map(it -> it.getPropertyPath().toString() + " " + it.getMessage())
+                .collect(Collectors.toList());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public final Iterable<String> handleMethodArgumentNotValid(MethodArgumentNotValidException e) {
+        return e.getBindingResult().getFieldErrors().stream()
+                .map(it -> it.getField() + " " + it.getDefaultMessage())
+                .collect(Collectors.toList());
+    }
 }
--- src/main/java/com/diboti/pricecomparatormarket/dto/incoming/ProductInCartDto.java
+++ src/main/java/com/diboti/pricecomparatormarket/dto/incoming/ProductInCartDto.java
@@ -1,10 +1,12 @@
 package com.diboti.pricecomparatormarket.dto.incoming;
 
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Value;
 
 @Value
 public class ProductInCartDto {
     @NotNull
+    @Size(min = 1, max = 100)
     String name;
 }
