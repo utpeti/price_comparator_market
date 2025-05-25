e0a8356d8c0bf03bbb326b858eef91dfd044d809
diboti <korposb@gmail.com>
2025-05-25 13:37:19 +0300

    added constrains

--- src/main/java/com/diboti/pricecomparatormarket/dto/incoming/ProductInCartDto.java
+++ src/main/java/com/diboti/pricecomparatormarket/dto/incoming/ProductInCartDto.java
@@ -1,8 +1,10 @@
 package com.diboti.pricecomparatormarket.dto.incoming;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Value;
 
 @Value
 public class ProductInCartDto {
+    @NotNull
     String name;
 }
--- src/main/java/com/diboti/pricecomparatormarket/dto/incoming/UserDataDto.java
+++ src/main/java/com/diboti/pricecomparatormarket/dto/incoming/UserDataDto.java
@@ -1,9 +1,17 @@
 package com.diboti.pricecomparatormarket.dto.incoming;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Value;
 
 @Value
 public class UserDataDto {
+    @NotNull
+    @Size(min = 1, max = 50)
     String email;
+
+    @NotNull
+    @Min(0)
     Double price;
 }
